7a52bae9620e1e9d1106133b7e3f5761fff5956c
kookoo <koo_g0kek@hotmail.com>
2019-10-20 21:17:50 +0700

    up

--- app.js
+++ app.js
@@ -7,13 +7,24 @@ var cors = require('cors')
 app.use(cors())
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/p', function (req, res, next) {
-  res.json({ msg: 'This is CORS-enabled for all origins!' })
+
+app.get('/xyz/:len', function (req, res, next) {
+  const len = req.params.len;
+  let result = [];
+  let current = 3;
+  result.push(current);
+
+  for (let i = 1; i < len; i++) {
+    current += i * 2
+    result.push(current);
+  }
+  res.json({ data: result })
 })
 
-app.get('/nearby', function (req, res, next) {
+app.get('/nearby/:position', function (req, res, next) {
+  const position = req.params.position;
   const axios = require('axios')
-  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=13.7699094, 100.58647930000006&radius=1000&type=restaurant&keyword=&key=AIzaSyBcqAN0A9zmWYC-tGYn8ggt2G9Nwx-cC-g`
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position}&radius=1000&type=restaurant&keyword=&key=AIzaSyBcqAN0A9zmWYC-tGYn8ggt2G9Nwx-cC-g`
   const result = axios.get(url, {
     headers: {
       'Access-Control-Allow-Origin': '*',
